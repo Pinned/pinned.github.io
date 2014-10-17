c3dbf3693d0f7d3695d485bd06a9765034b819d2
luozc <luozc@jpush.cn>
2014-10-17 19:50:02 +0800

    add Line number

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,6 +80,17 @@ $(document).on({
     afterPjax();
     hljs.initHighlighting.called = false;
     hljs.initHighlighting();
+    $('pre code').each(function(){
+        var lines = $(this).text().split('\n').length - 1;
+        var $numbering = $('<ul/>').addClass('pre-numbering');
+        $(this)
+            .addClass('has-numbering')
+            .parent()
+            .append($numbering);
+        for(i=1;i<=lines;i++){
+            $numbering.append($('<li/>').text(i));
+        }
+    });
   }
 });
 
