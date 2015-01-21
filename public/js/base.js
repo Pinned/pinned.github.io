bf23894bac7de86e70749f68b7f03d6cacb660bb
pinned <lovecluo@nightweaver.org>
2015-01-21 20:11:41 +0800

    change content

--- public/js/base.js
+++ public/js/base.js
@@ -41,11 +41,11 @@ function content_effects() {
   //remove the asidebar
   $('.row-offcanvas').removeClass('active');
   if ($("#nav").length > 0) {
-    $("#content > h2,#content > h3,#content > h4,#content > h5,#content > h6").each(function(i) {
+    $("#content > h1,#content > h2,#content > h3,#content > h4,#content > h5,#content > h6").each(function(i) {
       var current = $(this);
       current.attr("id", "title" + i);
       tag = current.prop('tagName').substr(-1);
-      $("#nav").append("<div style='margin-left:" + 15 * (tag - 1) + "px'><a id='link" + i + "' href='#title" + i + "'>" + current.html() + "</a></div>");
+      $("#nav").append("<div style='margin-left:" + 15 * (tag) + "px'><a id='link" + i + "' href='#title" + i + "'>" + "◆ " + current.html() + "</a></div>");
     });
     $("pre").addClass("prettyprint");
     prettyPrint();
