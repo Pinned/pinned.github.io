d0c0d19a61a9c344cc141fe2ccf89c9ccfaa9ab1
Pinned <lovecluo@gmail.com>
2023-05-30 19:10:38 +0800

    添加 snippet 页面

---
+++ scripts/snippet_helper.js
@@ -0,0 +1,17 @@
+hexo.extend.helper.register('code_snippet', function() {
+    // const sidebar = this.site.data.sidebar[type];
+    // const path = basename(this.path);
+    const posts = this.site.posts.data
+    let result = '';
+    result += '<div class="post-md">'
+    for (const item of Object.entries(posts)) {
+        console.log(item[1].title)
+        console.log(item[1].type)
+        if (item[1].type === 'snippet') {
+            result += `<h1><span class="prefix"></span><span class="content">${item[1].title}</span><span class="suffix"></span></h1>`
+            result += item[1].content
+        }
+    }
+    result += '</div>'
+    return result;
+  });
