a63903c4062ae4bcf391e6b8104bbb7c85065aae
luo <1103934424@qq.com>
2023-06-01 10:59:54 +0800

    修改图片 render 的逻辑

--- scripts/custom_markdown_render.js
+++ scripts/custom_markdown_render.js
@@ -1,4 +1,4 @@
-const { encodeURL,  url_for} = require('hexo-util');
+const { encodeURL, url_for } = require('hexo-util');
 hexo.extend.filter.register('marked:renderer', function (renderer) {
   // 使用方式参考文档： https://github.com/hexojs/hexo-renderer-marked/blob/master/README.md
   const { config } = this; // Skip this line if you don't need user config from _config.yml
@@ -6,25 +6,17 @@ hexo.extend.filter.register('marked:renderer', function (renderer) {
     return `<h${level}><span class="prefix"></span><span class="content">${text}</span><span class="suffix"></span></h${level}>`
   }
 
-  // renderer.image = function (href, title, text) {
-  //   const { relative_link } = hexo.config;
-  //   const { lazyload, prependRoot, postPath } = options;
-  //   if (!/^(#|\/\/|http(s)?:)/.test(href) && !relative_link && prependRoot) {
-  //     if (!href.startsWith('/') && !href.startsWith('\\') && postPath) {
-  //       const PostAsset = hexo.model('PostAsset');
-  //       // findById requires forward slash
-  //       const asset = PostAsset.findById(join(postPath, href.replace(/\\/g, '/')));
-  //       // asset.path is backward slash in Windows
-  //       if (asset) href = asset.path.replace(/\\/g, '/');
-  //     }
-  //     href = url_for.call(hexo, href);
-  //   }
-  //   let out = `<img src="${encodeURL(href)}"`;
-  //     if (text) out += ` alt="${text}"`;
-  //     if (title) out += ` title="${title}"`;
-  //     if (lazyload) out += ' loading="lazy"';
-  
-  //     out += '>';
-  //     return out;
-  // }
+  renderer.image = function (href, title, text) {
+    console.log(href)
+    let out = '<figure>'
+    out = `<img src="${encodeURL(href)}"`;
+    if (text) out += ` alt="${text}"`;
+    if (title) out += ` title="${title}"`;
+    out +=  ` referrerpolicy="no-referrer"`;
+    out += '>';
+    var className = hexo.config.image_caption.class_name || 'image-caption';
+    out += `<figcaption class="${className}">${text}</figcaption>`
+    out += '</figure>';
+    return out;
+  }
 })
